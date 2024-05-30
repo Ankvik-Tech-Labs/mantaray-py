a17826a2006e2786dd0dfd3bac2b4d886c757961
SAIKAT KARMAKAR <saikickkarma@gmail.com>
2024-05-30 13:34:11 +0530

    first commit

---
+++ src/mantaray_py/__init__.py
@@ -0,0 +1,10 @@
+"""Mantaray data structure in Python"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("mantaray-py")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
